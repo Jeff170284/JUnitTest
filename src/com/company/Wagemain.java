076cb1f10fdcfad69dd23f83daecf8671b84c07f
Jeff170284 <jeff.rodricks007@gmail.com>
2021-07-14 00:43:15 +0530

    JUnit Test

---
+++ src/com/company/Wagemain.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Wagemain {
+
+    public static void main(String[] args) {
+
+        ComputeWage obj = new ComputeWage();
+        obj.AcceptData();
+        obj.computeWage();
+        obj.display();
+    }
+}
